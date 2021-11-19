9218be9d67c49e2cad3a1bea353be6eda7bd777f
Tai Mai <maitruongtai22@gmail.com>
2021-11-19 16:05:59 +0000

    update source

--- source.cpp
+++ source.cpp
@@ -13,26 +13,8 @@ int main()
 	cout << "Nhap so nguyen 2: ";
 	cin >> b;
 <<<<<<< HEAD
-	cout << "Phep toan tru: ";
-	cin >> hieu(a, b);
+
 	system("pause");
 	return 0;
 }
-int hieu(int a, int b)
-{
-	return a-b;
-=======
-	cout << "Chon phep toan cong:  ";
-	cin >> tong(a, b);
-	system("pause");
-	return 0;
-}
-int tong(int a, int b)
-{
-	return a+b;
->>>>>>> c93c755df585efb2e46cf8fb193a4eb0d9a6064b
-}
-int nhan(int a, int b)
-{
-	return a*b;
-}
+
